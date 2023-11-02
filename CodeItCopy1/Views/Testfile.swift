e3b01b7d1672a7165827a608af3fca851630c6aa
Unionkyu <unionkyu@gmail.com>
2023-11-02 22:59:12 +0900

    //TestView

--- CodeItCopy1/Views/Testfile.swift
+++ CodeItCopy1/Views/Testfile.swift
@@ -1,26 +1,5 @@
 import SwiftUI
 
-//struct TextView: View {
-//  @State private var currentIndex: Int = 0
-//  var coursePages: [CoursePageMember]
-//
-//  var body: some View {
-//    ZStack {
-//      ForEach(0..<coursePages.count, id: \.self) { index in
-//        Testfile2(courseData: coursePages[index])
-//          .opacity(currentIndex == index ? 1 : 0)
-//          .scaleEffect(currentIndex == index ? 1.0 : 0)
-//          .offset(x: 0, y: (CGFloat(index - currentIndex) * 20))
-//          .zIndex(currentIndex == index ? 1 : 0)
-//      }
-//    }
-//  }
-//}
-//
-//
-//struct TextView_Previews: PreviewProvider {
-//  static var previews: some View {
-//    TextView(coursePages: pageData)
-//  }
-//}
-//
+struct testView {
+  var a: [[String]]
+}
